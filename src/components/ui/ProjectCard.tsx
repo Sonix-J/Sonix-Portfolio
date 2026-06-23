67d7162e38357edb88fe2d2bbbe81e98e9875340
Sonix-J <limosnerogabriel@gmail.com>
2026-06-23 09:46:49 +0800

    ProjectCard font size adjustments

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -12,8 +12,8 @@ export default function ProjectCard({ title, description, link }: ProjectCardPro
       rel="noopener noreferrer"
       className="block border border-gray-100  p-4 shadow-sm bg-white"
     >
-      <h3 className="font-bold text-xl">{title}</h3>
-      <p className="text-gray-500 mt-1">{description}</p>
+      <h3 className="font-bold text-md">{title}</h3>
+      <p className="text-gray-500 text-sm mt-1">{description}</p>
     </a>
   )
 }
