bfdf0ee818b4801ad70959517ed8412a16aa8849
Sonix-J <limosnerogabriel@gmail.com>
2026-06-23 09:25:24 +0800

    Added 'Education' section

---
+++ src/components/sections/Education.tsx
@@ -0,0 +1,14 @@
+import Card from "../ui/Card";
+
+export default function Education() {
+  return (
+    <Card title="Education">
+      <p className="font-semibold">
+        Bachelor of Science in Information Technology — CTU - Main Campus
+      </p>
+      <p className="text-sm text-gray-500">
+        August 2023 - August 2026
+      </p>
+    </Card>
+  );
+}
