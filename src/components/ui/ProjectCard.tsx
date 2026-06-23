8b7744cd6f6f7c985d4c71e3538cd38f639018cd
Sonix-J <limosnerogabriel@gmail.com>
2026-06-23 13:52:26 +0800

    'ProjectCard' Aadjustments

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -16,17 +16,20 @@ export default function ProjectCard({
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col border border-gray-100 overflow-hidden"
+      className="flex flex-col bg-gray-200 rounded overflow-hidden"
     >
       {preview && (
         <img src={preview} alt={title} className="w-full h-36 object-cover" />
       )}
       <div className="flex flex-col gap-2 p-4">
-        <h3 className="font-bold text-md">{title}</h3>
+        <div className="flex flex-row gap-2 justify-between items-center">
+          <h3 className="font-bold text-md">{title}</h3>
 
-        <span className="text-xs px-2 py-1 font-semibold bg-gray-200 self-start">
-          {link}
-        </span>
+          <span className="text-xs px-2 py-1 font-semibold border rounded bg-white self-start">
+            {link}
+          </span>
+        </div>
+
         <p className="text-sm text-gray-800">{description}</p>
       </div>
     </a>
